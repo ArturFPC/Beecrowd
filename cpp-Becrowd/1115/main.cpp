a3ddc08a822bb5177faa757f6eabde06d892a38a
ArturFPC <arturfpc@gmail.com>
2023-07-18 15:31:42 -0300

    18-07-23

---
+++ cpp-Becrowd/1115/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x,y = 1;
+    while (x != 0 && y !=0){
+        cin>>x>>y;
+        if (x>0 && y>0){
+            cout<<"primeiro"<<endl;
+        }
+        if (x>0 && y<0){
+            cout<<"quarto"<<endl;
+        }
+        if (x<0 && y>0){
+            cout<<"segundo"<<endl;
+        }
+        if (x<0 && y<0){
+            cout<<"terceiro"<<endl;
+        }
+    }
+    return 0;
+}
